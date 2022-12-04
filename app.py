17ec138e5abad5e5e21764ca6bb2157b0d32d805
MuhammadAlgshy <m.algshy@live.com>
2022-12-04 00:27:04 +0000

    Create Test API with user inputs

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from getData import getData
 from settings import *
 import time
 from scheduleTask import scheduleTask
+import datetime
+
 def main():
  while True:
     try:
@@ -9,8 +11,8 @@ def main():
           1- Test DB  \n \
           2- Test API \n \
           3- Test Schedule Function \n \
-          3- Run API without Schedule \n \
-          4- Exit!")
+          4- Run API without Schedule \n \
+          5- Exit!")
 
         # if I am using Python 10 I could use Match function to create the switch
         # Get user input
@@ -23,20 +25,27 @@ def main():
         if programe == 3:
           scheduleTask("")
         if programe == 4:
+          print("1 -> Use defualt(ISO3 = AFG, Start date = 2022-12-01, End date = 2022-12-02 ) \n2 -> Enter your own data")
+          scenario= int(input("Scenario: "))
+          if scenario ==1:
+               getData("production")
+          elif scenario==2:
+            Iso3_entry = input('Enter ISO3: ')
+            start_date_entry = input('Enter a start date in YYYY-MM-DD format: ')
+            year, month, day = map(int, start_date_entry.split('-'))
+            start_date = datetime.date(year, month, day)
+            end_date_entry = input('Enter a end date in YYYY-MM-DD format: ')
+            year, month, day = map(int, end_date_entry.split('-'))
+            end_date = datetime.date(year, month, day)
+            print("ISO3 => "+Iso3_entry+"\nStart Date =>" + str(start_date) +"\nEnd Date =>" + str(end_date))
+            getData("production", Iso3_entry, start_date, end_date )
+
+        if programe == 5:
           print("Thank you for using our system! \n looking forward to see you soon! ^_^")
           exit()  
     except ValueError:
           print('Please select one of the programes')
           time.sleep(1)
-    # Schedule the job to run every day at 6:00
-   # 
-    # For Testing you can try the 10 Sec 
-    # schedule.every(10).seconds.do(getData)
-    
-    # Run the job continuously
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
 
 if __name__ == '__main__':
     main()
--- getData.py
+++ getData.py
@@ -4,16 +4,18 @@ import json
 #Settings
 from settings import logging
 
-#Variables
-api_URL= 'https://api.hungermapdata.org/v1/foodsecurity/country/AFG/region?date_start=2022-01-01&date_end=2022-02-01'
 
-def getData(status):
-   
+def getData(status, iso3='AFG', start_date='2022-12-01', end_date='2022-12-02'):
+   #Variables
+    api_URL= f'https://api.hungermapdata.org/v1/foodsecurity/country/{iso3.isupper()}/region?date_start={start_date}&date_end={end_date}'
+    print(api_URL)
     try:
         response_API= requests.get(api_URL)
         data = response_API.text
         if status == "production":
-            parse_json = json.loads(data)   
+            parse_json = json.loads(data)  
+            with open('data.json', 'w') as f:
+                     json.dump(parse_json, f)
         logging.info(response_API.status_code)
     except:
         logging.error("ERROR: Failed to establish connection, please check your API URL!")
